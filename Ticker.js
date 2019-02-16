fd2b60858b05db52bdc6d981c09d74d08f5eb084
qymqa <33643078+qymqa@users.noreply.github.com>
2019-02-17 00:30:11 +0500

    Add files via upload

---
+++ Ticker.js
@@ -0,0 +1,19 @@
+﻿function Ticker() {
+
+    this._i = 0
+
+};
+
+Ticker.prototype = {
+
+    tick: function() {
+
+        console.log(this._i++);
+
+    }
+
+}
+
+var ticker = new Ticker();
+
+setInterval(ticker.tick.bind(ticker), 1000)
